2bc6fcb83dc9e73c614a717a719fbf5079ab81d2
Victory23 <mengweiwang@163.com>
2020-11-01 22:20:04 +0800

    fix：自定义字段属性扩展！

--- tests/OSharp.Tests/Entity/EntityBaseTests.cs
+++ tests/OSharp.Tests/Entity/EntityBaseTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Linq;
+using System.Reflection;
 using Shouldly;
 
 using Xunit;
@@ -16,20 +17,59 @@ namespace OSharp.Entity.Tests
             guidKeyEntity.Id.ShouldBeOfType<Guid>();
             guidKeyEntity.Id.ShouldBe(Guid.Empty);
         }
+        private class GuidKeyEntity : EntityBase<Guid> { }
+
 
         [Fact]
         public void Equals_Test()
         {
-            TestEntity1 entity1 = new TestEntity1() { Id = 1 };
-            TestEntity1 entity2 = new TestEntity1() { Id = 1 };
-            entity2.Equals(entity1).ShouldBeTrue();
-            entity2.Equals(this).ShouldBeFalse();
-            entity2.GetHashCode().ShouldBe(entity1.GetHashCode());
+            TestEntity1 entity1 = new TestEntity1() { Id = 1, EncryptTelNo = "123456" };
+
+            FixData(entity1);
+
+            var temp = "123456";
+
+            ////转化加密数据
+            //context.Add(entity1);
         }
 
-        private class GuidKeyEntity : EntityBase<Guid> { }
 
+        private static void FixData<T>(T input)
+        {
+            Type type = input.GetType();
+            PropertyInfo[] propertyInfos = type.GetProperties();
+            foreach (var propItem in propertyInfos.Where(p => p.IsDefined(typeof(ColumnEncryptAttribute), false)))
+            {
+                ColumnEncryptAttribute attribute = (ColumnEncryptAttribute)propItem.GetCustomAttribute(typeof(ColumnEncryptAttribute), false);
+                //attribute.Name;
+                string parameterName = propItem.Name;
 
-        private class TestEntity1 : EntityBase<int> { }
+                //get值
+                var tempValue = propItem.GetValue(input)?.ToString();
+
+                //加密方式
+                var ss = tempValue + tempValue;
+
+                //set值
+                propItem.SetValue(input, ss);
+            }
+        }
+
+        private class TestEntity1 : EntityBase<int>
+        {
+            [ColumnEncrypt("MD5")]
+            public string EncryptTelNo { get; set; }
+        }
+
     }
+
+
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class ColumnEncryptAttribute : Attribute
+    {
+        public string Name { get; set; }
+        public ColumnEncryptAttribute(string value) { Name = value}
+    }
+
+
 }
